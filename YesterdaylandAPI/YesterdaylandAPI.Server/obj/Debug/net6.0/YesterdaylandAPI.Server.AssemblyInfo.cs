163476b46b28491beb8b2ca789c5317f62b0a2b6
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-05-26 05:12:39 -0300

    Fix: trying to fix connection with swagger
    Fix: trying to fix connection with swagger

--- YesterdaylandAPI/YesterdaylandAPI.Server/obj/Debug/net6.0/YesterdaylandAPI.Server.AssemblyInfo.cs
+++ YesterdaylandAPI/YesterdaylandAPI.Server/obj/Debug/net6.0/YesterdaylandAPI.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YesterdaylandAPI.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88165f77dad6d6457ccd0ba564ff1e3f7da85675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16cfe2ba2148d3197d44044f5b09693fcd9bf213")]
 [assembly: System.Reflection.AssemblyProductAttribute("YesterdaylandAPI.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YesterdaylandAPI.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
