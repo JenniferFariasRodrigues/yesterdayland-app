be2ef4343be019786ba112f979bf482077fd3b6f
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-05-26 22:35:59 -0300

    feat: ticket-list.component, app-routing,app--modules on the frontend

--- YesterdaylandAPI/yesterdaylandapi.client/src/app/components/customer-tickets/customer-tickets.component.ts
+++ YesterdaylandAPI/yesterdaylandapi.client/src/app/components/customer-tickets/customer-tickets.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Customer } from '../../models/customer.model';
+import { Ticket } from '../../models/ticket.model';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-customer-tickets',
   templateUrl: './customer-tickets.component.html',
   styleUrl: './customer-tickets.component.css'
 })
-export class CustomerTicketsComponent {
 
+export class CustomerTicketsComponent implements OnInit {
+  customers: Customer[] = [];
+  selectedCustomer: Customer | undefined;
+  tickets: Ticket[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getCustomers().subscribe((customers: Customer[]) => {
+      this.customers = customers;
+    });
+  }
+
+  loadTickets(): void {
+    if (this.selectedCustomer) {
+      this.tickets = this.selectedCustomer.tickets;
+    }
+  }
 }
